a9bacd1056cab1bed1667910954f9ad504b27a6b
acspacelit <161153689+acspacelit@users.noreply.github.com>
2024-06-18 20:43:27 +0000

    anp

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,76 @@
 import streamlit as st
 import pandas as pd
-import math
-from pathlib import Path
+import os
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP Dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
+def calculate_percentile_score(column):
+    percentiles = column.rank(pct=True)
+    scores = percentiles.apply(lambda x: 1 if x <= 0.25 else (2 if x <= 0.5 else (3 if x <= 0.75 else 4)))
+    return scores
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
+def reassign_segment(row):
+    if row['Puntuacion_Avance_Temporal'] in [3, 4] and row['Puntuacion_NoDesembolsos'] in [3, 4] and row['Puntuacion_Porcentaje_Desembolsado'] in [3, 4]:
+        return 'Finalizando en Buen estado'
+    elif row['Puntuacion_Avance_Temporal'] in [3, 4] and row['Puntuacion_NoDesembolsos'] in [3, 4] and row['Puntuacion_Porcentaje_Desembolsado'] in [1, 2]:
+        return 'Necesitara Extensión o Desembolso Abrupto'
+    elif row['Puntuacion_Avance_Temporal'] in [3, 4] and row['Puntuacion_NoDesembolsos'] in [1, 2] and row['Puntuacion_Porcentaje_Desembolsado'] in [3, 4]:
+        return 'Finalizando en Buen estado'
+    elif row['Puntuacion_Avance_Temporal'] in [3, 4] and row['Puntuacion_NoDesembolsos'] in [1, 2] and row['Puntuacion_Porcentaje_Desembolsado'] in [1, 2]:
+        return 'Necesitara Extensión o Desembolso Abrupto'
+    elif row['Puntuacion_Avance_Temporal'] in [1, 2] and row['Puntuacion_NoDesembolsos'] in [3, 4] and row['Puntuacion_Porcentaje_Desembolsado'] in [3, 4]:
+        return 'Desembolso Anticipado'
+    elif row['Puntuacion_Avance_Temporal'] in [1, 2] and row['Puntuacion_NoDesembolsos'] in [3, 4] and row['Puntuacion_Porcentaje_Desembolsado'] in [1, 2]:
+        return 'Potencialmente bueno'
+    elif row['Puntuacion_Avance_Temporal'] in [1, 2] and row['Puntuacion_NoDesembolsos'] in [1, 2] and row['Puntuacion_Porcentaje_Desembolsado'] in [3, 4]:
+        return 'Desembolso Anticipado'
+    elif row['Puntuacion_Avance_Temporal'] in [1, 2] and row['Puntuacion_NoDesembolsos'] in [1, 2] and row['Puntuacion_Porcentaje_Desembolsado'] in [1, 2]:
+        return 'Proyecto Joven'
+    else:
+        return 'Otros'
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+st.title('Análisis de Desembolsos')
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+# Define the path to the Excel file
+file_path = os.path.join('data', 'anp.xlsx')
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
-
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
+# Check if the file exists
+if os.path.exists(file_path):
+    data = pd.read_excel(file_path)
+    
+    st.write("Datos cargados del archivo de análisis:")
+    st.write(data.head())
+    
+    # Calculate scores for each of the specified columns
+    data['Puntuacion_Avance_Temporal'] = calculate_percentile_score(data['Avance Temporal'])
+    data['Puntuacion_NoDesembolsos'] = calculate_percentile_score(data['NoDesembolsos'])
+    data['Puntuacion_Porcentaje_Desembolsado'] = calculate_percentile_score(data['Porcentaje Desembolsado'])
+    
+    # Concatenate the scores
+    data['Puntuacion_Concatenada'] = (
+        data['Puntuacion_Avance_Temporal'].astype(str) +
+        data['Puntuacion_NoDesembolsos'].astype(str) +
+        data['Puntuacion_Porcentaje_Desembolsado'].astype(str)
     )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP Dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
-
-
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[gdp_df['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
+    
+    # Assign segments based on the concatenated scores
+    data['Segmento'] = data.apply(reassign_segment, axis=1)
+    
+    st.write("Datos con puntuaciones y segmentos:")
+    st.write(data.head())
+    
+    # Option to download the updated data
+    output_file = 'analysis_anp_updated.xlsx'
+    data.to_excel(output_file, index=False)
+    
+    with open(output_file, "rb") as file:
+        btn = st.download_button(
+            label="Descargar archivo con análisis",
+            data=file,
+            file_name=output_file,
+            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         )
+else:
+    st.write("Asegúrate de que el archivo analysis_anp.xlsx esté en la carpeta 'data'.")
+
+    data = data['Estado Gop'] = "VIGENTE"
+    st.write("Datos Vigentes con segmentos:")
+    st.write(data.head())
